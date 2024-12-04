12e13cd3d0fe1872094dcfe6a496734634e0b434
jellis1997 <jellis21@mail.wou.edu>
2024-12-03 21:34:07 -0800

    DockerfileJE

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+bash
+#!/bin/bash
+
+# A simple script that reads a configuration file and prints a message
+CONFIG_FILE="/my_solution/myconfig.conf"
+
+if [ -f "$CONFIG_FILE" ]; then
+    echo "Configuration file found. Reading configuration..."
+    cat "$CONFIG_FILE"
+else
+    echo "Configuration file not found!"
+fi
+
+echo "Hello my name is Jarrett, and this is my script."
